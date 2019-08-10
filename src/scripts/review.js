3dc0eb83503d5d518ceb903c01e0125738b767b1
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-08-09 23:38:20 -0500

    made ratings cat faces

--- src/scripts/review.js
+++ src/scripts/review.js
@@ -3,7 +3,7 @@ import elFactory from "./factory.js";
 const createReviewComponent = (reviewObj, productIdNum) => {
   const reviewSection = elFactory("section", { classList: ["review-section", "product--" + productIdNum] })
   const reviewer = elFactory("h3", {classList: ["reviewer"]}, reviewObj.reviewer)
-  const stars = elFactory("div", {}, "rating: " + reviewObj.stars)
+  const stars = elFactory("div", {}, "rating: " + "😺".repeat(reviewObj.stars))
   const review = elFactory("p", {classList: ["review"]}, reviewObj.review)
   const reviewDiv = document.querySelector("#product-reviews--" + productIdNum)
   reviewDiv.appendChild(reviewSection)
